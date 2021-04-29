773920b43ad3c1c7ab99c27e80072baa123168d1
DESKTOP-8TBEVD4\Krystian Izdebski <krystian.izdebski1999@gmail.com>
2021-04-29 12:17:39 +0200

    Other example of property binding

--- my-app/src/app/home/home.component.ts
+++ my-app/src/app/home/home.component.ts
@@ -6,8 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  homeTitle = "Welcome to the homepage..."
-  textData = "This is from .ts"
+  homeTitle = "Welcome to the homepage...";
+  textData = "This is from .ts";
+  textDisabled = true;
   constructor() { }
 
   ngOnInit(): void {
